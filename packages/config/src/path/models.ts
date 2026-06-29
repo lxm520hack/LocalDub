47910051eb1dd322f02392e784d4a6fcd2be140b
aa <Nahida-aa@outlook.com>
2026-06-29 22:59:58 +0800

    feat: 移除不必要的模型路径定义，优化路径管理

--- packages/config/src/path/models.ts
+++ packages/config/src/path/models.ts
@@ -7,7 +7,10 @@ export const MODEL_CACHE_DIR = env.MODEL_CACHE_DIR;
 export const DEMUCS_MODEL_DIR = join(MODEL_CACHE_DIR, 'demucs');
 
 export const WHISPER_MODEL_DIR = join(MODEL_CACHE_DIR, 'whisper');
-const OPENAI_WHISPER_MODEL_DIR = join(homedir(), '.cache', 'whisper');
-export const SHERPA_WHISPER_DIR = join(MODEL_CACHE_DIR, 'sherpa-whisper-turbo');
+
+// openai-whisper 自己管理
+// export const OPENAI_WHISPER_MODEL_DIR = join(homedir(), '.cache', 'whisper');
+ 
+export const SHERPA_WHISPER_DIR = join(WHISPER_MODEL_DIR, 'sherpa_onnx');
 
 export const VOXCPM_MODEL_DIR = join(MODEL_CACHE_DIR, 'voxcpm2');
